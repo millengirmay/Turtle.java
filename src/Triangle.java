aee268e84597debd835c856f1e1c932b066ac15f
Student <millenkahsaygirmay@gmail.com>
2025-05-23 14:57:54 -0700

    Initial commit

---
+++ src/Triangle.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+    public class Triangle extends Shape{
+        private int width;
+        Triangle(Turtle turtle, int xPosition, int yPosition, String colorName, int borderWidth, int width){
+            super( turtle, xPosition, yPosition, colorName, borderWidth);
+            this.width = width;
+        }
+        @Override
+        public void draw() {
+            super.draw();
+
+            turtle.forward(width);
+            turtle.turnRight(120);
+
+            turtle.forward(width);
+            turtle.turnRight(120);
+
+            turtle.forward(width);
+            turtle.turnRight(120);
+
+        }
+    }
+

e65136485192f7355c71fd815f618b3bc95f6e88
Student <millenkahsaygirmay@gmail.com>
2025-05-23 15:08:12 -0700

    Initial commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,27 @@
-public class Circle {
 
-}
+import java.awt.*;
+
+    public class Circle extends Shape{
+        private int width;
+        Circle(Turtle turtle, int xPosition, int yPosition, String colorName, int borderWidth, int width){
+            super( turtle, xPosition, yPosition, colorName, borderWidth);
+            this.width = width;
+        }
+        @Override
+        public void draw() {
+            super.draw();
+
+            int sides = 12;
+            int angle = 360 / sides;
+            int length = (int) (width / Math.PI);
+
+            for(int s=0; s<sides; s++){
+                turtle.forward(length);
+                turtle.turnRight(angle);
+            }
+
+        }
+    }
+
+
+

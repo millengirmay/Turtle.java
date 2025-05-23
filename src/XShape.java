bffc1c09f87ae95c8a0440db87ff606d78c0ebdb
Student <millenkahsaygirmay@gmail.com>
2025-05-23 15:00:54 -0700

    Initial commit

---
+++ src/XShape.java
@@ -0,0 +1,35 @@
+
+import java.awt.*;
+
+    public class XShape extends Shape{
+        private int width;
+        private int height;
+        XShape(Turtle turtle,int xPosition,int yPosition,String colorName,int borderWidth, int width, int height){
+            super( turtle, xPosition, yPosition, colorName, borderWidth);
+            this.width = width;
+            this.height = height;
+        }
+        @Override
+        public void draw() {
+            super.draw();
+
+            // calculate the hypotenuse (diagonal)
+            // a2 + b2 = c2
+            double widthSquared = Math.pow(width, 2);
+            double heightSquared = Math.pow(height, 2);
+            double hypotenuse = Math.sqrt(widthSquared + heightSquared);
+
+            turtle.setColor(Color.GREEN);
+
+            turtle.turnRight(45);
+            turtle.forward(hypotenuse);
+
+            turtle.penUp();
+            turtle.goTo(width / 2.0, height / 2.0);
+            turtle.turnRight(90);
+
+            turtle.penDown();
+            turtle.forward(hypotenuse);
+        }
+    }
+
